67ed52849c0811f9b47cb630f62e01f59dfceda1
SaiKrupaReddy20 <37209645+SaiKrupaReddy20@users.noreply.github.com>
2019-07-18 15:22:41 +0530

    Add files via upload

---
+++ client/src/components/Publications/publications.js
@@ -0,0 +1,66 @@
+import React, {Component,Fragment} from 'react';
+
+export default class publications extends Component {	
+
+	state = {
+		nfields : 1,
+		fields : [
+			{
+				id: 0,
+				pubtitle: "",
+				pubplace: "",
+				pubdescription: ""
+			}
+		]
+	}
+
+	render() {
+		const { fields } = this.state;
+		return (
+			<Fragment>
+				{fields.map( (field) => (
+					<div className="form-inline" key={field.id}>
+						<input type="text" className="form-control" name={"pubtitle"} placeholder="Title"
+							onChange = { (e) => {
+								field.pubtitle = e.target.value;
+								this.props.publicationsData(fields);
+							} }
+						/>
+						<input type="text" className="form-control" name={"pubplace"} placeholder="Place of Publication"
+							onChange = { (e) => {
+								field.pubplace = e.target.value;
+								this.props.publicationsData(fields);
+							} }
+						/>
+						<textarea name={"pubdescription"} placeholder="About the Publication"
+							onChange = { (e) => {
+								field.pubdescription = e.target.value;
+								this.props.publicationsData(fields);
+							} }
+						/>
+						<button className="btn btn-default btn-danger" onClick={ (e) => {
+							e.preventDefault();
+							this.setState(state => ({
+								fields: state.fields.filter(f => f.id !== field.id)
+							}));
+							this.props.publicationsData(this.state.fields.filter(f => f.id!==field.id));
+						} }>Remove</button>
+					</div>	
+					) )}
+				<button className="btn btn-default btn-primary" onClick={ (e) => {
+					e.preventDefault();
+					var newField = {
+							id: this.state.nfields,
+							pubtitle: "",
+							pubplace: "",
+							pubdescription: ""
+						};
+					this.setState(state => ({
+						nfields : state.nfields + 1,
+						fields: [...state.fields,newField]
+					}));
+				}}>Add</button>
+			</Fragment>
+		)
+	}
+}
