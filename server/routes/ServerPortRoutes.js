2b11e0263c59172f40881f7613733520f763f6ff
CS17B011 <cs17b011@iittp.ac.in>
2019-07-20 15:21:08 +0530

    Added PDF generation

--- server/routes/ServerPortRoutes.js
+++ server/routes/ServerPortRoutes.js
@@ -1,14 +1,28 @@
-
 const express = require('express')
 const app = express();
 const ServerPortRouter = express.Router();
 app.use('/serverport', ServerPortRouter);
 
+const latex = require('node-latex');
+const fs = require('fs'); 
+const path = require('path');
+
 ServerPortRouter.route('/').post(function (req, res) {
     console.log(req.body);
+    
+    //Input latex file
+    const input = fs.createReadStream(path.join( __dirname , 'input.tex'));
+    //Name of output File
+	const output = fs.createWriteStream(path.join( __dirname , 'output.pdf'));
+	//Object for pdf generation
+	const pdf = latex(input);
 
-    res.send({"data":"Data is Received by Server, Frontend-Backend are connected"});
-
+	//Piping out object
+	pdf.pipe(output);
+	pdf.on('error', err => console.error(err));
+	pdf.on('finish', () => console.log('PDF generated!'));
+	
+	res.send(pdf);
 });
 
 
