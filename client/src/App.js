41d97cc2b38588744650e56ea6a4fb08f5b6c1a6
SaiKrupaReddy20 <37209645+SaiKrupaReddy20@users.noreply.github.com>
2019-07-27 11:16:19 +0530

    added footer

--- client/src/App.js
+++ client/src/App.js
@@ -10,11 +10,23 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <h2 align="center">Resume Generator</h2>
-        {/*<DynamicForm
-          />*/}
-          <FormCombined />
+        <div className="Resume">
+          <h2 align="center">Resume Generator</h2>
+            {/*<DynamicForm
+              />*/}
+              <FormCombined />
         </div>
+        <div style={{background: '#ccd1c7',margin: '10px', padding: '10px'}} className="Footer">
+          <h3 align="center">Created and Designed by</h3>
+          <div align="center" className="Names">
+            <a href="https://github.com/shubhamrsangle">ShubhamSangle  </a>
+            <a href="https://github.com/CS17B011">Deep  </a>
+            <a href="https://github.com/nandha1nks">Nandhkumar  </a>
+            <a href="https://github.com/me17b016">RajGarg  </a>
+            <a href="https://github.com/SaiKrupaReddy20">SaiKrupaReddy  </a>
+          </div>
+        </div>
+      </div>
     );
   }
 }
